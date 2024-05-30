ad7407d2425db15aeb67de0a903acb5ef7ab116a
3N14C <3n14c@vk.com>
2024-05-30 21:00:27 +0800

    Update axios-config.ts

--- src/config/axios-config.ts
+++ src/config/axios-config.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://threelegant.vercel.app/api/api"
+      ? "https://threelegant.vercel.app/api"
       : "http://localhost:3400/api",
 });
