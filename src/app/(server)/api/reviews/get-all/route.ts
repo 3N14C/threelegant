bfe1f9ca5ffd93901ac4e213d3a981ff270a1dd0
3N14C <3n14c@vk.com>
2024-06-16 13:09:43 +0800

    Update route.ts

--- src/app/(server)/api/reviews/get-all/route.ts
+++ src/app/(server)/api/reviews/get-all/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma-client";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export const GET = async () => {
     const reviews = await prisma.review.findMany();
 
